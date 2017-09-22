7c893ee56bb6538bb99e8ec26922e540e94f5989
Julia.Vaseva <julia.vaseva@jetbrains.com>
2017-09-22 11:41:18 +0300

    Main method

--- EmptyProject/EmptyProject/Program.cs
+++ EmptyProject/EmptyProject/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace EmptyProject
+﻿using System;
+using System.Data;
+
+namespace EmptyProject
 {
-    public static class Program
-    {
-        public static void Main(string[] args)
+        public static class Program
         {
-            
+            public static void Main(string[] args)
+            {
+                DataTable table1 = new DataTable("patients");
+                table1.Columns.Add("name");
+                table1.Columns.Add("id");
+                table1.Rows.Add("james", 1);
+                table1.Rows.Add("john", 2);
+
+                DataTable table2 = new DataTable("patients2");
+                table2.Columns.Add("id2");
+                table2.Columns.Add("name2");
+                table2.Rows.Add(1, "Lucy");
+                table2.Rows.Add(2, "cathy");
+
+                // Create a DataSet and put both tables in it.
+                DataSet set = new DataSet("office");
+                set.Tables.Add(table1);
+                set.Tables.Add(table2);
+
+                // Visualize DataSet.
+                Console.WriteLine(set.GetXml());
+            }
         }
-    }
 }
